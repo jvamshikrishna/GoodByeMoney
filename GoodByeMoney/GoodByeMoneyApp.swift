058894c627c55b40875ab8eff644f6c5d7fa28c1
jvamshikrishna <jvamshikrishna1997@gmail.com>
2024-01-15 17:45:54 -0600

    Initial Commit

---
+++ GoodByeMoney/GoodByeMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodByeMoneyApp.swift
+//  GoodByeMoney
+//
+//  Created by Vamshi Krishna on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct GoodByeMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
