637d7f3c1486a57084a82f3de548ac4376d744f5
jvamshikrishna <jvamshikrishna1997@gmail.com>
2024-01-21 20:50:09 -0600

    categorires screen made

---
+++ GoodByeMoney/Model/Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  GoodByeMoney
+//
+//  Created by Vamshi Krishna on 1/21/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct Category: Identifiable{
+    var id: Int
+    var name: String
+    var color: Color
+    
+}
---
+++ GoodByeMoney/Screens/Categories.swift
@@ -0,0 +1,18 @@
+//
+//  Categories.swift
+//  GoodByeMoney
+//
+//  Created by Vamshi Krishna on 1/15/24.
+//
+
+import SwiftUI
+
+struct Categories: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Categories()
+}
